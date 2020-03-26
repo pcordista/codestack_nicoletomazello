df4c48a24ab21d3e73862db9c90c4764c07ff35f
Ezequiel Cazella <ezequiel@crearepp.com.br>
2020-03-26 01:16:17 -0300

    Adicionado Arquivos do WordPress

--- index.php
+++ index.php
@@ -1,51 +1,17 @@
 <?php
-$file = basename($_SERVER['PHP_SELF']); // your file name 
-$last_modified_time = filemtime($file);
-$etag = md5_file($file);
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-header("Last-Modified: " . gmdate("D, d M Y H:i:s", $last_modified_time) . " GMT");
-header("Etag: $etag");
-?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-<?php include 'header.php'; ?>
-
-<?php include 'components/menu.php'; ?>
-
-<?php include 'components/sidebar.php'; ?>
-
-<?php include 'components/slider.php'; ?>
-
-<?php include 'components/newsletter.php'; ?>
-
-<?php include 'components/about.php'; ?>
-
-<?php include 'components/parallax.php'; ?>
-
-<?php include 'components/services.php'; ?>
-
-<?php include 'components/testemonials.php'; ?>
-
-<?php include 'components/purpose.php'; ?>
-
-<?php include 'components/news.php'; ?>
-
-<?php include 'components/events.php'; ?>
-
-<?php include 'components/contact.php'; ?>
-
-<?php include 'components/instagram.php'; ?>
-
-<?php include 'components/modal.php'; ?>
-
-<?php include 'footer.php'; ?>
-
-
-<?php include 'assets/js/JS_includes.php'; ?>
-<script src="assets/devlop/live.js"></script>
-<script>
-    document.addEventListener("load", startHome())
-</script>
-
-</body>
-
-</html>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
