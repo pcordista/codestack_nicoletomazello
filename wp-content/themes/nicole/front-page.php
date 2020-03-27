86ba6389d95ecd3566c5eb9049a2277fb8ba60eb
http_cs_01 <ezequiel@ultima7.com.br>
2020-03-27 13:50:17 -0300

    ajustes front

--- wp-content/themes/nicole/front-page.php
+++ wp-content/themes/nicole/front-page.php
@@ -12,7 +12,7 @@ get_header();
 
         <?php
 
-        get_template_part('template-parts/content', 'menu');
+//         get_template_part('template-parts/content', 'menu');
 
         get_template_part('template-parts/content', 'sidebar');
 
